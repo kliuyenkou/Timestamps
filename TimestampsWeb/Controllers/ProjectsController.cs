0b12c69a54d9fd094c4e1033988107c933d36b39
kliuyenkou <klyuenkov@gmail.com>
2016-11-22 15:30:49 +0300

    Add form for creating Project.

---
+++ TimestampsWeb/Controllers/ProjectsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using TimestampsWeb.Models;
+
+namespace TimestampsWeb.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Projects
+        public ActionResult Create()
+        {
+            return View("");
+        }
+    }
+}
---
+++ TimestampsWeb/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TimestampsWeb.ViewModels
+{
+    public class ProjectViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
