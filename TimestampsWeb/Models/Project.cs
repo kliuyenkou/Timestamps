cd15eae1f3a9e777c670275b5cee8aabd36c775b
kliuyenkou <klyuenkov@gmail.com>
2016-11-22 13:32:07 +0300

    Create the Project model.

---
+++ TimestampsWeb/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimestampsWeb.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(127)]
+        public string Title { get; set; }
+
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required]
+        public ApplicationUser Creator { get; set; }
+
+    }
+}
