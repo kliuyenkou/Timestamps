7de5406bbf18e82c97d5a50db51ca1761448be3a
kliuyenkou <klyuenkov@gmail.com>
2016-11-24 13:09:58 +0300

    Added HourageViewModel.

--- TimestampsWeb/Controllers/HourageController.cs
+++ TimestampsWeb/Controllers/HourageController.cs
@@ -1,8 +1,10 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
 using TimestampsWeb.Models;
+using TimestampsWeb.ViewModels;
 
 namespace TimestampsWeb.Controllers
 {
@@ -13,30 +15,22 @@ namespace TimestampsWeb.Controllers
         // GET: Hourage
         public ActionResult Index()
         {
-            var hourages = db.Hourages.Include(h => h.Project).Include(h => h.User);
+            var userId = User.Identity.GetUserId();
+
+            // This solution with bad perfomance, but it's ok for testing interfaces
+            var projectsUserTakePart = db.ProjectNominations.Include(pn => pn.Project).Where(pn => pn.UserId == userId).Select(pn=>pn.Project);
+            var hourages = db.Hourages.Include(h => h.Project).Include(h => h.User).Where(h => projectsUserTakePart.Contains(h.Project));
             return View(hourages.ToList());
         }
 
-        // GET: Hourage/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Hourage hourage = db.Hourages.Find(id);
-            if (hourage == null)
-            {
-                return HttpNotFound();
-            }
-            return View(hourage);
-        }
-
+    
         // GET: Hourage/Create
         public ActionResult Create()
         {
-            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Title");
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Name");
+            var userId = User.Identity.GetUserId();
+            var projectsUserTakePart = db.ProjectNominations.Include(pn => pn.Project).Where(pn => pn.UserId == userId).Select(pn => pn.Project);
+            ViewBag.ProjectId = new SelectList(projectsUserTakePart, "Id", "Title");
+            ViewBag.UserId = userId;
             return View();
         }
 
@@ -45,55 +39,31 @@ namespace TimestampsWeb.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,ProjectId,UserId,WorkDescripton,Date,Hours")] Hourage hourage)
+        public ActionResult Create( HourageViewModel viewModel)
         {
+            var userId = User.Identity.GetUserId();
+            
             if (ModelState.IsValid)
             {
+                var hourage = new Hourage()
+                {
+                    WorkDescripton = viewModel.WorkDescripton,
+                    Date = viewModel.Date,
+                    Hours = viewModel.Hours,
+                    UserId = userId,
+                    ProjectId = viewModel.ProjectId,
+                };
+
                 db.Hourages.Add(hourage);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Title", hourage.ProjectId);
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Name", hourage.UserId);
-            return View(hourage);
+            var projectsUserTakePart = db.ProjectNominations.Include(pn => pn.Project).Where(pn => pn.UserId == userId).Select(pn => pn.Project);
+            ViewBag.ProjectId = new SelectList(projectsUserTakePart, "Id", "Title");
+            return View(viewModel);
         }
 
-        // GET: Hourage/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Hourage hourage = db.Hourages.Find(id);
-            if (hourage == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Title", hourage.ProjectId);
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Name", hourage.UserId);
-            return View(hourage);
-        }
-
-        // POST: Hourage/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,ProjectId,UserId,WorkDescripton,Date,Hours")] Hourage hourage)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(hourage).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Title", hourage.ProjectId);
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Name", hourage.UserId);
-            return View(hourage);
-        }
-
+   
         // GET: Hourage/Delete/5
         public ActionResult Delete(int? id)
         {
--- TimestampsWeb/ViewModels/HourageViewModel.cs
+++ TimestampsWeb/ViewModels/HourageViewModel.cs
@@ -6,12 +6,12 @@ namespace TimestampsWeb.ViewModels
 {
     public class HourageViewModel
     {
-        public int Id { get; set; }
-
         [StringLength(128)]
         public string WorkDescripton { get; set; }
         [Required]
+        public int ProjectId { get; set; }
         public Project Project { get; set; }
+
         [Required]
         public DateTime Date { get; set; }
         public double Hours { get; set; }
